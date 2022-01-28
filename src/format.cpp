aa019b4808bc17eb0c2a44a26ebe4ac392db2493
Ujjwal Dalmia <ujjwal.dalmia@elroyair.com>
2022-01-28 10:22:42 -0800

    Format time to HH:MM:SS

--- src/format.cpp
+++ src/format.cpp
@@ -1,11 +1,20 @@
 #include <string>
+#include <iomanip>
 
 #include "format.h"
 
 using std::string;
 
-// TODO: Complete this helper function
+// DONE: Complete this helper function
 // INPUT: Long int measuring seconds
 // OUTPUT: HH:MM:SS
-// REMOVE: [[maybe_unused]] once you define the function
-string Format::ElapsedTime(long seconds[[maybe_unused]]) { return string(); }
+string Format::ElapsedTime(long seconds) {
+  int hours = seconds / 3600;
+  seconds = seconds % 3600;
+  int minutes = seconds / 60;
+  seconds = seconds % 60;
+  string output;
+  std::stringstream stream;
+  stream << std::setw(2) << std::setfill('0') << hours << ":" << std::setw(2) << std::setfill('0') << minutes << ":" << std::setw(2) << std::setfill('0') << seconds;
+  return stream.str();
+}
